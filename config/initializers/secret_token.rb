5faeba5b47e67f2145c3bb67e7101dd4d9c41795
Mikronad <comrade_a@ukr.net>
2013-05-16 17:35:25 +0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TerPAApp::Application.config.secret_token = '245bca3a02fe72c548a975b53dec3076d527cf2ff0b85b1200cc11997ff3269e34fe884f26d4b829e9be37fabf570e5a47adc61a7c6d8d1275b2eb867b3322b4'
